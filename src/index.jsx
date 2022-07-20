7547aff19fa9c769b117b72e9954f72de357d3f7
Darshak Parikh <darshak@protonmail.com>
2022-07-20 08:32:30 +0530

    Normalize file extensions

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import App from "./components/App.js";
+import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
-import { getStore } from "./store/store.js";
+import { getStore } from "./store/store";
 import "./styles.sass";
-import { startInterval } from "./util.js";
+import { startInterval } from "./util";
 
 (async () => {
   const container = document.getElementById("root");
