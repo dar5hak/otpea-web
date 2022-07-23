0a99a93edc143dc885956a60d9a968606c19c64f
Darshak Parikh <darshak@protonmail.com>
2022-07-23 22:26:17 +0530

    Remove Redux dependency

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
-import { getStore } from "./store/store";
 import "./styles.sass";
 
-(async () => {
-  const container = document.getElementById("root");
-  const root = createRoot(container);
-  const store = await getStore();
-
-  root.render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-})();
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
 
 serviceWorker.register();
